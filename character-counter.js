d22251ba1beb49d3c7dc217098a4719ec1a90f57
Britta Jorgenson <bljorgen@gmail.com>
2019-05-01 18:36:27 +0000

    Fixed variable definition and semicolons in character-counter.js

--- character-counter.js
+++ character-counter.js
@@ -1,11 +1,11 @@
 function countLetters(text) {
-  text = text.toLowerCase().split(" ").join("")
+  text = text.toLowerCase().split(" ").join("");
   letterCounts = {};
 
   for (var i = 0; i < text.length; i++) {
     currentLetter = text[i];
     if (letterCounts[currentLetter]) {
-      letterCounts[currentLetter]++
+      letterCounts[currentLetter]++;
     } else {letterCounts[currentLetter] = 1}
   }
 
