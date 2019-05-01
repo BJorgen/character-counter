0c5f6db5625d011b48739c78a7a08d2e19469e0e
Britta Jorgenson <bljorgen@gmail.com>
2019-05-01 18:32:06 +0000

    Functionality added to character-counter.js

---
+++ character-counter.js
@@ -0,0 +1,15 @@
+function countLetters(text) {
+  text = text.toLowerCase().split(" ").join("")
+  letterCounts = {};
+
+  for (var i = 0; i < text.length; i++) {
+    currentLetter = text[i];
+    if (letterCounts[currentLetter]) {
+      letterCounts[currentLetter]++
+    } else {letterCounts[currentLetter] = 1}
+  }
+
+  return letterCounts;
+}
+
+console.log(countLetters("lighthouse in the house"));
